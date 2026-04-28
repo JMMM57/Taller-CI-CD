185e3ae0402751901c764b26cf836a55e98c59b4
JMMM57 <Juanmamtz572@gmail.com>
2026-04-28 11:24:35 -0600

    Ejecutar prebas jest

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Esta es una prueba sencilla.');
 });
 
 app.get('/suma', (req, res) => {
