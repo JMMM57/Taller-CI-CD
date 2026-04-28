699e46e9cb57cf6a8a903f1bc05bd6430f126600
JMMM57 <Juanmamtz572@gmail.com>
2026-04-28 11:28:21 -0600

    Ejecutar prebas jest y prueba suma

--- app.test.js
+++ app.test.js
@@ -5,3 +5,8 @@ test('GET / devuelve mensaje', async () => {
     const res = await request(app).get('/');
     expect(res.text).toBe('Esta es una prueba sencilla.');
 });
+
+test('GET /suma devuelve la suma de a y b', async () => {
+    const res = await request(app).get('/suma?a=5&b=3');
+    expect(res.body.resultado).toBe(8);
+});
